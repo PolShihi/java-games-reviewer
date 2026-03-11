ad02233c94e0d7bdd63d4a5f1ebb72d4fb2d21b0
PolShihi <zhora.lyamtsev@mail.ru>
2026-03-11 16:52:11 +0300

    code refactoring for depricated functionality is done

--- frontend-angular-app/src/app/core/http/http-client.providers.ts
+++ frontend-angular-app/src/app/core/http/http-client.providers.ts
@@ -2,6 +2,6 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiErrorInterceptor } from './api-error.interceptor';
 import { baseUrlInterceptor } from './base-url.interceptor';
 
-export const HTTP_CLIENT_PROVIDERS = [
-  provideHttpClient(withInterceptors([baseUrlInterceptor, apiErrorInterceptor])),
-];
+export const HTTP_CLIENT_PROVIDERS = provideHttpClient(
+  withInterceptors([baseUrlInterceptor, apiErrorInterceptor])
+);
