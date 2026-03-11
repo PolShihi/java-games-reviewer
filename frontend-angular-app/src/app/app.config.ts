f14686ae37f22876a321567d0133ae840ba8c765
PolShihi <zhora.lyamtsev@mail.ru>
2026-03-11 14:53:41 +0300

    package json was changed, main page with filters is done

--- frontend-angular-app/src/app/app.config.ts
+++ frontend-angular-app/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
@@ -8,6 +12,7 @@ import { HTTP_CLIENT_PROVIDERS } from './core/http/http-client.providers';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimations(),
     provideRouter(routes),
     ...HTTP_CLIENT_PROVIDERS,
