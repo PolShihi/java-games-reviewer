e0e853f43a697fe545a74fad79c93e5d7cf0d3f8
PolShihi <zhora.lyamtsev@mail.ru>
2026-03-13 17:45:37 +0300

    docker are done

---
+++ frontend-angular-app/docker-entrypoint.d/50-envsubst.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+
+API_BASE_URL="${API_BASE_URL:-http://localhost:8088/api/v1}"
+ENABLE_LOGS="${ENABLE_LOGS:-false}"
+
+export API_BASE_URL ENABLE_LOGS
+
+envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.js
